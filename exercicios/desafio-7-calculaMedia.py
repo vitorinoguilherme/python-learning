3db945c5e46e2229f92463b57ffd50ccd2706289
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 13:46:17 -0300

    Criado script calculaMedia.py

---
+++ exercicios/desafio-7-calculaMedia.py
@@ -0,0 +1,12 @@
+'''
+ calculaMedia.py - Dado duas notas calcula média.
+ autor: Guilherme Vitorino
+ 02.04.20 - 13h44min
+'''
+
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+
+media = float(nota1 + nota2) / 2
+
+print(f"A média: {media:.2f}")
