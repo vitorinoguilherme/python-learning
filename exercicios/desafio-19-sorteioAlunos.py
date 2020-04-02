3ad9e721e317abaafead41e9f728d7330792428e
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 17:05:07 -0300

    Criado script sorteioAlunos.py

---
+++ exercicios/desafio-19-sorteioAlunos.py
@@ -0,0 +1,16 @@
+'''
+ sorteioAlunos.py - Dado o ângulo cálcula seno, cosseno e tangente.
+ autor: Guilherme Vitorino
+ 02.04.20 - 15h57min
+'''
+
+from random import randint
+
+alunos = []
+
+for i in range(4):
+    nome = input("Informe o nome do aluno: ")
+    alunos.insert(i, nome)
+
+print(f"Aluno escolhido para apagar quadro: {alunos[randint(0, 3)]}")
+
