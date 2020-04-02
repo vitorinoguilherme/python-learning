34096af68abdb2a6f7d20dde94136c95da639e96
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 13:15:13 -0300

    Criado exemplo primeiroScript

---
+++ exemplos/primeiroScript.py
@@ -0,0 +1,9 @@
+'''
+ primeiroScript.py - Imprime nome, idade e peso.
+'''
+
+nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+peso = input("Qual é o seu peso? ")
+
+print(f"\nNome:{nome}, idade:{idade} anos, peso:{peso}kg")
