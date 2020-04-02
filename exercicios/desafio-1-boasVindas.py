bd853794b0a9b5392dc3113bad27234c609c640c
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 13:24:24 -0300

    Criado script boasVindas.py

---
+++ exercicios/desafio-1-boasVindas.py
@@ -0,0 +1,9 @@
+'''
+ boasVindas.py - Saudação de boas vindas
+ autor: Guilherme Vitorino
+ 02.04.20 - 13h20min
+'''
+
+nome = input("Informe o nome: ")
+
+print(f"\nOlá {nome}! Prazer em te conhecer!")
