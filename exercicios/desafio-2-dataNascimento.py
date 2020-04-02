0db726b3a9cae9928c36cf3ea3deab911e429c4e
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 13:32:52 -0300

    Criado script dataNascimento.py

---
+++ exercicios/desafio-2-dataNascimento.py
@@ -0,0 +1,11 @@
+'''
+ dataNascimento.py - Informa a data de nascimento.
+ autor: Guilherme Vitorino
+ 02.04.20 - 13h26min
+'''
+
+dia = int(input("Dia: "))
+mes = int(input("Mês: "))
+ano = int(input("Ano: "))
+
+print(f"\nVocê nasceu na data {dia:02}/{mes:02}/{ano:04}. Correto?\n")
