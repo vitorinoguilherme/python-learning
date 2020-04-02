a70fe4fa1c64833ed16e92a8ae6eb2857fb4e6c5
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 13:41:26 -0300

    Criado script sucessorAntecessor.py

---
+++ exercicios/desafio-5-sucessorAntecessor.py
@@ -0,0 +1,11 @@
+'''
+ sucessorAntecessor.py - Antecessor e Sucessor do numero informado.
+ autor: Guilherme Vitorino
+ 02.04.20 - 13h39min
+'''
+
+numero = int(input("Informe um número: "))
+
+print(f"\nO antecessor: {numero - 1}\n"
+      f"O número: {numero}\n"
+      f"O sucessor: {numero + 1}")
