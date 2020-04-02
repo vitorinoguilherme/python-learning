19436b8a5fb5a0c2194b79f91fba9a0e7b603fa5
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 14:34:37 -0300

    Criado script teste bibliotecaRandom

---
+++ testes/bibliotecaRandom.py
@@ -0,0 +1,11 @@
+'''
+ bibliotecaRandom.py - Demostra funcionalidades da biblioteca random.
+ autor: Guilherme Vitorino
+ 02.04.20 - 14h34min
+'''
+
+import random
+
+number = random.randint()
+
+print(number)
