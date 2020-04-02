2904b3811cad9b7166f778cbafa9029ab053d7ed
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 13:35:18 -0300

    Criado script somaNumeros.py

---
+++ exercicios/desafio-3-somaNumeros.py
@@ -0,0 +1,10 @@
+'''
+ somaNumeros.py - soma valores informados.
+ autor: Guilherme Vitorino
+ 02.04.20 - 13h33min
+'''
+
+number1 = int(input("Informe o primeiro numero: "))
+number2 = int(input("Informe o segundo numero: "))
+
+print(f"A soma entre {number1} e {number2} é: {number1 + number2}")
