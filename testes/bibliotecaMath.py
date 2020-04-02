ecc4c1de658bb1bb1ac22c7d7ec65270f18ae198
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-02 14:33:10 -0300

    Criado script teste bibliotecaMath

---
+++ testes/bibliotecaMath.py
@@ -0,0 +1,11 @@
+'''
+ bibliotecaMath.py - Demostra funcionalidades da biblioteca math.
+ autor: Guilherme Vitorino
+ 02.04.20 - 14h31min
+'''
+
+from math import sqrt, ceil
+
+number = int(input("Informe um numero: "))
+
+print(f"Raiz do numero {number} = {ceil(sqrt(number))}")
