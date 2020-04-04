33f3c359b970bb0c58b0bb625c51cc0340f5ece9
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-04 20:12:16 -0300

    Aprimorado lógica digitosSeparados.py

--- exercicios/desafio-23-digitosSeparados.py
+++ exercicios/desafio-23-digitosSeparados.py
@@ -6,11 +6,16 @@
 
 numero = int(input("Informe um número: "))
 
-if( numero < 0 or numero > 9999 ):
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+
+if numero < 0 or numero > 9999:
     print(f"O número deve estar entre 0-9999")
 else:
-    numero = str(numero)
-    print(f"Unidade: {numero[3]}\n"
-          f"Dezena:  {numero[2]}\n"
-          f"Centena: {numero[1]}\n"
-          f"Milhar:  {numero[0]}\n")
+    print(f"Unidade: {u}\n"
+          f"Dezena:  {d}\n"
+          f"Centena: {c}\n"
+          f"Milhar:  {m}\n")
+
