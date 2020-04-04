c07aaea787af21da3ee239c3bf0429d010af6a2d
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-04 16:50:45 -0300

    Criado exemplo aula 09 - Manipulação de Strings

---
+++ exemplos/aula09.py
@@ -0,0 +1,7 @@
+frase = '   Aprendizagem Java Curso em Vídeo   '
+
+frase = frase.strip()
+frase = frase.replace('Java', 'Python')
+
+print('\n', frase)
+print(f' Quantidade caracteres: {len(frase)}')
