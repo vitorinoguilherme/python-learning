8a3dac1bfb8c9b80a2d91241a63b2e6567a981dd
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-08 11:13:02 -0300

    Formatado texto

--- curso-02-python/exercicios/desafio-43-calculaIMC.py
+++ curso-02-python/exercicios/desafio-43-calculaIMC.py
@@ -6,17 +6,18 @@
 from math import pow
 
 cores = {'vermelho': '\033[1;31m', 'verde': '\033[1;32m', 'magenta': '\033[1;35m'}
-peso = float(input("Informe seu peso: kg "))
-altura = float(input("Informe sua altura: m "))
+peso = float(input("Informe seu peso: (kg) "))
+altura = float(input("Informe sua altura: (m) "))
 imc = peso / pow(altura, 2)
 
+print(f"Seu IMC é {imc:.1f}, ", end='')
 if imc <= 18.5:
-    print(f"Seu IMC é {imc:.2f}, {cores['vermelho']}Você está abaixo do peso.")
+    print(f"{cores['vermelho']}Você está ABAIXO do peso.")
 elif 18.5 < imc <= 25:
-    print(f"Seu IMC é {imc:.2f}, {cores['verde']}Você tem o peso ideal.")
+    print(f"{cores['verde']}Você tem o peso NORMAL.")
 elif 25 < imc <= 30:
-    print(f"Seu IMC é {imc:.2f}, {cores['magenta']}Você está com sobrepeso.")
+    print(f"{cores['magenta']}Você está com SOBREPESO.")
 elif 30 < imc <= 40:
-    print(f"Seu IMC é {imc:.2f}, {cores['vermelho']}Você com obesidade.")
+    print(f"{cores['vermelho']}Você está com OBESIDADE.")
 elif imc > 40:
-    print(f"Seu IMC é {imc:.2f}, {cores['vermelho']}Você está com obesidade mórbida.")
+    print(f"{cores['vermelho']}Você está com OBESIDADE MÓRBIDA.")
