610310ca0362ee1c1563e65793ef95086b74282f
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-04-08 14:25:08 -0300

    Criado script verificaPalindromo.py

---
+++ curso-02-python/exercicios/desafio-53-verificaPalindromo.py
@@ -0,0 +1,22 @@
+'''
+ verificaPalindromo.py -
+ autor: Guilherme Vitorino
+ 08.04.20 - 13h40min
+'''
+
+print(f"{' INFORMA SE A FRASE É UM PALÍNDROMO ':=^50}")
+
+frase = str(input("Digite a frase: ")).strip()
+
+frase = frase.split()  # Separa em palavras
+frase = ''.join(frase)  # Junta frase sem espaços
+
+inverteFrase = []
+for i in range(len(frase) - 1, -1, -1):
+    inverteFrase.append(frase[i])
+inverteFrase = ''.join(inverteFrase)  # Junta frase invertida sem espaços
+
+if frase == inverteFrase:
+    print("\033[1;32mA frase é um PALÍNDROMO! Podemos ler ao contrário e manter o significado.")
+else:
+    print("\033[1;31mA frase não é um PALÍNDROMO.")
